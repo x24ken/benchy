c062b598a68f3f6ce05d50aa3b6293ec031675d1
qwukker <minor7addfloortom@gmail.com>
2024-11-15 07:08:30 -0600

    progress

---
+++ src/components/multi_tool_call/ToolCallExpectationList.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="toolcallexpectationlist-w">ToolCallExpectationList.vue</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped>
+.toolcallexpectationlist-w {
+}
+</style>
