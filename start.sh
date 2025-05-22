aadfffd4777a749618260399774d481fcc002750
qwukker <minor7addfloortom@gmail.com>
2025-05-22 09:28:01 -0500

    reorg

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Start the dev server in the background
-bun run dev &
+(cd client && bun run dev) &
 
-# Change directory and start Python server
-cd server && uv run --env-file .env python server.py
+# Start Python server
+(cd server && uv run --env-file .env python server.py) &
 
 # Wait for both processes
 wait
