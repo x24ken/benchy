328156ff3c17d390d5a5211d9f747abdd13dd585
qwukker <minor7addfloortom@gmail.com>
2025-05-24 10:58:12 -0500

    progress

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ import UnoCSS from 'unocss/vite'
 export default defineConfig({
   plugins: [vue(), UnoCSS()],
   server: {
-    port: 5174,
+    port: 5173,
     proxy: {
       '/prompt': {
         target: 'http://127.0.0.1:5000',
